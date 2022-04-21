21d6d84578749c50a9c6beeff3fddbeb82e47b88
rxavio <rucahoxavi@gmail.com>
2022-04-21 12:18:59 +0200

    console province and district

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const { Provinces, Districts } = require('rwanda');
 const app = express()
-
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
+console.log(Provinces()); // [ 'East', 'Kigali', 'North', 'South', 'West' ]
+console.log(Districts()); // [ 'Bugesera', 'Gatsibo', 'Kayonza', 'Kirehe',...]
 app.listen(3000)
 console.log('app running on port: 3000 ');
